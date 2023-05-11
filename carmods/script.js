62d65fad20bdec0e60a3378ee3422a0f9ce12cd7
yungsamd17 <64147848+yungsamd17@users.noreply.github.com>
2023-05-11 20:58:43 +0200

    🔧 BIG overhaul/cleanup
    Couldn't be bothered to make multiple commit's, so here is a shit ton of stuff.

---
+++ carmods/script.js
@@ -0,0 +1,128 @@
+// Loading Overlay
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    var loadingOverlay = document.getElementById("loadingOverlay");
+    loadingOverlay.style.opacity = 0;
+    setTimeout(function () {
+      loadingOverlay.style.display = "none";
+    }, 125); // Delay to hide the overlay after the opacity transition
+  }, 250); // Delay of # second before hiding the overlay
+});
+
+// Search
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  var ulList = document.querySelectorAll("ul#modList"); // Select all <ul> elements with id="modList"
+
+  ulList.forEach(function (ul) {
+    var children = ul.querySelectorAll("li");
+    children.forEach(function (child) {
+      child.style.display = "none";
+    });
+
+    li = ul.querySelectorAll("li");
+    li.forEach(function (li) {
+      a = li.getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li.style.display = "";
+      }
+    });
+  });
+
+  var otherElements = document.querySelectorAll("h2, h3, a:not([id='searchInput'])");
+  otherElements.forEach(function (element) {
+    var parentUL = element.closest("ul#modList");
+    var isExcluded = false;
+
+    // Check if element is excluded from hiding
+    if (element.tagName === "A") {
+      var ancestorH1 = element.closest("h1");
+      var ancestorHeader = element.closest("header");
+      var ancestorP = element.closest("p");
+
+      if (ancestorH1 || ancestorHeader || (ancestorP && ancestorP.contains(element))) {
+        isExcluded = true;
+      }
+    }
+
+    if (!parentUL && !isExcluded) {
+      element.style.display = "none";
+    } else {
+      element.style.display = "";
+    }
+  });
+
+  var searchResultsCount = document.querySelectorAll("ul#modList li:not([style='display: none;'])").length;
+  var messageElement = document.getElementById("searchResultsMessage");
+  messageElement.textContent = "Found " + searchResultsCount + " matching search results.";
+}
+
+function handleKeyPress(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    performSearch();
+  }
+}
+
+function performSearch() {
+  var input, filter, searchQuery;
+  input = document.getElementById("searchInput");
+  filter = input.value.trim();
+  searchQuery = encodeURIComponent(filter);
+
+  // Update URL with search query
+  var newURL = window.location.protocol + "//" + window.location.host + window.location.pathname + "?search=" + searchQuery;
+  window.location.href = newURL;
+}
+
+function clearSearchInput() {
+  var input = document.getElementById("searchInput");
+  input.value = "";
+  performSearch(); // Trigger the search functionality with an empty query
+}
+
+function loadSearchResults() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var searchQuery = urlParams.get("search");
+  if (searchQuery) {
+    var input = document.getElementById("searchInput");
+    input.value = decodeURIComponent(searchQuery);
+    myFunction(); // Trigger search functionality
+    document.getElementById("clearSearchButton").style.display = "block"; // Show clear search button
+  } else {
+    document.getElementById("clearSearchButton").style.display = "none"; // Hide clear search button
+  }
+}
+
+// Call loadSearchResults on page load
+window.onload = function () {
+  loadSearchResults();
+  var input = document.getElementById("searchInput");
+  input.addEventListener("keypress", handleKeyPress);
+  input.addEventListener("input", handleSearchInputChange);
+
+  var clearButton = document.getElementById("clearSearchButton");
+  clearButton.addEventListener("click", clearSearchInput);
+};
+
+// Open links in new tab
+function openLinksInNewTab(elementId) {
+  const ulElements = document.querySelectorAll(`ul#${elementId}`);
+  ulElements.forEach(function (ulElement) {
+    const links = ulElement.getElementsByTagName('a');
+    for (let i = 0; i < links.length; i++) {
+      links[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        if (links[i].hasAttribute('href') && links[i].getAttribute('href') !== '') {
+          window.open(links[i].href, '_blank');
+        }
+      });
+    }
+  });
+}
+
+// Call the function with the element ID
+openLinksInNewTab("modList");
