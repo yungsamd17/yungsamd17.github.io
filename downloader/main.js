a454d2b511654f367f01f7c21776924d11792be6
yungsamd17 <yungsamd@proton.me>
2023-09-23 00:36:42 +0200

    🔧Updated downloader

--- downloader/main.js
+++ downloader/main.js
@@ -1,8 +1,8 @@
 window.onload = function () {
 	const error = text => {
-		document.title = "Downloader - Failed";
+		document.title = "Downloader Failed";
 		document.body.innerHTML = text;
 	};
-	if (!window.location.search) error("No Parameters");
+	if (!window.location.search) error("Nothing to Download found");
 	else window.DownloadApi.convert(window.location.search, error);
 };
